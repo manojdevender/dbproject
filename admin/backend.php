5e188029851b7133dac0576ebef39082c24e1fcb
manojdevender <manojdevender@gmail.com>
2016-04-20 00:50:22 +0530

    add movie form in admin and movie catalogue added

--- admin/backend.php
+++ admin/backend.php
@@ -21,7 +21,9 @@ $adminRow=mysql_fetch_array($res);
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome - <?php echo $adminRow['email']; ?></title>
-<link rel="stylesheet" href="../css/style.css" type="text/css" />
+ <!-- Bootstrap -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../css/backend.css" type="text/css" />
 </head>
 <body background="../images/background.jpg" />
 <div id="header">
@@ -34,27 +36,141 @@ $adminRow=mysql_fetch_array($res);
         </div>
     </div>
 </div>
-
-<div id="body">
-    <center>
-        <div id="adminform">
-        <form method="post">
-        <table align="center" width="30%" border="0">
-        <tr>
-        <center>
-            <p>
-                <b>[ Message here ]</b>
-              <textarea placeholder="Message for users!" name="message"></textarea><br>
-              <input type="submit" value="Save" name="msg"><br><?php echo $confirm; ?>
-            </p>
-        </center>
-        </tr>
-        </table>
-        </form>
-        </div>
+<center>
+<p>
+    <textarea placeholder="Message for users!" name="message"></textarea>
+    <input type="submit" value="Save" name="msg"><?php echo $confirm; ?>
+</p>
 </center>
-	<br />
+<form class="form-horizontal">
+<fieldset>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="name">Movie Name</label>  
+  <div class="col-md-4">
+  <input id="name" name="name" type="text" placeholder="Movie name" class="form-control input-md" required="">
+    
+  </div>
 </div>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="mid">Movie ID</label>  
+  <div class="col-md-4">
+  <input id="mid" name="mid" type="number" placeholder="Movie ID" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Directorfirstname">Directorfirstname</label>  
+  <div class="col-md-4">
+  <input id="Directorfirstname" name="Directorfirstname" type="text" placeholder="Directorfirstname" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Directorlastname">Directorlastname</label>  
+  <div class="col-md-4">
+  <input id="Directorlastname" name="Directorlastname" type="text" placeholder="Directorlastname" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="did">Director ID</label>  
+  <div class="col-md-4">
+  <input id="did" name="did" type="number" placeholder="Director ID"  class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="year">Release Date</label>  
+  <div class="col-md-4">
+  <input id="year" name="year" type="number" placeholder="Release Date" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Actorfirstame">Actorfirstame</label>  
+  <div class="col-md-4">
+  <input id="Actorfirstame" name="Actorfirstame" type="text" placeholder="Actorfirstame" class="form-control input-md">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Actorlastame">Actorlastame</label>  
+  <div class="col-md-4">
+  <input id="Actorlastame" name="Actorlastame" type="text" placeholder="Actorlastame" class="form-control input-md">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="id">id</label>  
+  <div class="col-md-4">
+  <input id="id" name="id" type="text" placeholder="id" class="form-control input-md">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="gender">Gender</label>  
+  <div class="col-md-4">
+  <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+  <input type="radio" name="gender" value="other"> Other
+  </div>
+</div>
+
+<!-- Select Basic -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="genre">Genre</label>
+  <div class="col-md-4">
+    <select id="genre" name="genre" class="form-control">
+      <option value="-1">Select</option>
+      <option value="1">Action</option>
+      <option value="2">Biography</option>
+      <option value="3">Comedy</option>
+      <option value="4">Crime</option>
+      <option value="5">Drama</option>
+      <option value="6">Documentary</option>
+      <option value="7">Family</option>
+      <option value="8">Fantasy</option>
+      <option value="9">History</option>
+      <option value="10">Horror</option>
+      <option value="11">Music</option>
+      <option value="12">Mystery</option>
+      <option value="13">News</option>
+      <option value="14">RealityTV</option>
+      <option value="15">Romance</option>
+      <option value="16">Sci-fi</option>
+      <option value="17">Short</option>
+      <option value="18">Sport</option>
+      <option value="19">Talkshow</option>
+      <option value="20">Thriller</option>
+      <option value="21">War</option>
+      <option value="22">Western</option>
+      <option value="23">Animation</option>
+      <option value="24">Adventure</option>
+    </select>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Addingmovie"></label>
+  <div class="col-md-4">
+    <button id="Addmovie" name="Addmovie" class="btn btn-success">Add Movie</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
 
 </body>
 </html>
--- home.php
+++ home.php
@@ -18,7 +18,9 @@ $disp = mysql_fetch_array($msg_query);
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome - <?php echo $userRow['email']; ?></title>
-<link rel="stylesheet" href="css/style.css" type="text/css" />
+ <!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/home.css" type="text/css" />
 </head>
 <body background="images/background.jpg">
 <div id="header">
@@ -33,9 +35,82 @@ $disp = mysql_fetch_array($msg_query);
 </div>
 
 <div id="body">
-	<br />
-    <p>Message is :<?php echo $disp['message']; ?></p>
-    <br />
+<p>Message is :<?php echo $disp['message']; ?></p>
+<div class="container bootstrap snipets">
+   <h1 class="text-center text-muted">Popular Movies</h1>
+   <div class="row flow-offset-1">
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-1.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-2.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-3.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-4.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-5.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-6.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-7.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-8.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+     <div class="col-xs-6 col-md-4">
+       <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-9.jpg" alt=""></a>
+         <div class="caption">
+           <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+             <del>$24.99</del></span><span class="price sale">$12.49</span>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
 </div>
 
 </body>
